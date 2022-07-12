0cea3ca59975e655aa35586b83bb8104716e20ab
jinsumin <suminjin@naver.com>
2022-07-12 15:51:08 +0900

    add popular jobs section

--- src/pages/jobs.jsx
+++ src/pages/jobs.jsx
@@ -1,9 +1,11 @@
 import Layout from "../components/layout";
+import PopularJobs from "../components/popularJobs";
 
 export default function Jobs() {
   return (
     <Layout>
       <h1>알바 페이지</h1>
+      <PopularJobs />
     </Layout>
   );
 }
